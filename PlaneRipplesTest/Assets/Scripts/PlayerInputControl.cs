8008e35c9b85877b506795831eb518559877e854
glacius3000 <glacius3000@hotmail.com>
2014-02-25 15:39:45 -0500

    Input script
    Quick change so that I dont look dumb

--- PlaneRipplesTest/Assets/Scripts/PlayerInputControl.cs
+++ PlaneRipplesTest/Assets/Scripts/PlayerInputControl.cs
@@ -1,15 +1,23 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class PlayerInputControl : MonoBehaviour {
+public class PlayerInputControl : MonoBehaviour 
+{
+	private WavesHandler waveScript;
 
 	// Use this for initialization
-	void Start () {
-	
+	void Start () 
+	{
+		waveScript = GameObject.Find("WavePlane").GetComponent<WavesHandler>();
 	}
 	
 	// Update is called once per frame
-	void Update () {
-	
+	void Update () 
+	{
+		if(Input.GetMouseButton(0))
+		{
+
+		}
 	}
+
 }
