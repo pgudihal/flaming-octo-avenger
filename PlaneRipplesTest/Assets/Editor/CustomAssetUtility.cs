0ac85abe66cc2075491a1923c57cabfd4009b420
pgudihal <pratik.gudihal@gmail.com>
2014-03-12 17:17:47 -0400

    Adding NGUI 3.5 before starting new UI implementation (will prune this later to reduce size)

---
+++ PlaneRipplesTest/Assets/Editor/CustomAssetUtility.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using UnityEditor;
+using System.IO;
+
+public static class CustomAssetUtility
+{
+	public static void CreateAsset<T> () where T : ScriptableObject
+	{
+		T asset = ScriptableObject.CreateInstance<T>();
+		
+		string path = AssetDatabase.GetAssetPath (Selection.activeObject);
+		if (path == "")
+		{
+			path = "Assets";
+		}
+		else if (Path.GetExtension (path) != "")
+		{
+			path = path.Replace (Path.GetFileName (AssetDatabase.GetAssetPath (Selection.activeObject)), "");
+		}
+		
+		string assetPathAndName = AssetDatabase.GenerateUniqueAssetPath (path + "/New " + typeof(T).ToString() + ".asset");
+		
+		AssetDatabase.CreateAsset (asset, assetPathAndName);
+		
+		AssetDatabase.SaveAssets ();
+		EditorUtility.FocusProjectWindow ();
+		Selection.activeObject = asset;
+	}
+
+	//all paths are relative to the assets folder. no need to specify that its in the assets folder
+	public static void CreateAssetAtPath<T> (string path, string AssetName,T asset) where T : ScriptableObject
+	{
+		//T asset = ScriptableObject.CreateInstance<T>();
+
+		path = "Assets" + "/" + path;
+		string assetPathAndName = AssetDatabase.GenerateUniqueAssetPath(path + "/" + typeof(T).ToString() + "-" + AssetName + ".asset");
+
+		AssetDatabase.CreateAsset(asset,assetPathAndName);
+		AssetDatabase.SaveAssets();
+	}
+}
