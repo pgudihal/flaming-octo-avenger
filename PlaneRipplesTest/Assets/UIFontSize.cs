4a1899ba0619a93b887e5824523d765f046080e9
pgudihal <pratik.gudihal@gmail.com>
2014-04-13 17:01:59 -0400

    UI Band-aid
    Fixed UI being too small and out of position. Need to come back later
    and redo resolution/font sizes before release

---
+++ PlaneRipplesTest/Assets/UIFontSize.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class UIFontSize : MonoBehaviour {
+	Resolution curRes;
+	public UIFont radioStars;
+	public UILabel score, timer;
+	// Use this for initialization
+	void Start () {
+		curRes = Screen.currentResolution;
+		if (curRes.height < 800 && curRes.width < 1280){
+			score.fontSize = 40;
+			timer.fontSize = 40;
+		}
+		else{
+			score.fontSize = 53;
+			timer.fontSize = 40;
+		}
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
